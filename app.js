8735e9ef7b13ad9b19853f7dffa9694153a8e7b8
Duy Nguyen <martinnguyen0618@gmail.com>
2015-05-01 21:57:43 -0700

    sahglkhas

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ Instagram.set('client_id', INSTAGRAM_CLIENT_ID);
 Instagram.set('client_secret', INSTAGRAM_CLIENT_SECRET);
 
 //connect to database
-mongoose.connect(process.env.MONGODB_CONNECTION_URL);
+mongoose.connect(process.env.MONGOLAB_URI);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function (callback) {
